eff2ff121188e92da51988f5ed9aa4725abb75a6
John Doe <kamkilljoy@gmail.com>
2025-02-25 15:01:39 +0400

    Скорректировал все тесты и проверки

--- Tests.py
+++ Tests.py
@@ -28,36 +28,6 @@ def test_practice_form(browser):
     with allure.step("Введение сообщения"): practice_form.enter_message()
     with allure.step("Отправка формы"): practice_form.click_submit()
 
-'''
-    with allure.step("Проверка наличия окна с результатами"):
-        assert practice_form.check_modal_result()
-    with allure.step("Проверка корректного имени"):
-        assert (practice_form.read_result_modal_student_name().casefold() ==
-            f"{user["firstname"]} {user["lastname"]}".casefold())
-    with allure.step("Проверка корректного email"):
-        assert (practice_form.read_result_modal_student_email().casefold() ==
-            user["email"].casefold())
-    with allure.step("Проверка корректного гендера"):
-        assert (practice_form.read_result_modal_student_gender().casefold() ==
-            user["gender"].casefold())
-    with allure.step("Проверка корректного мобильного номера"):
-        assert (practice_form.read_result_modal_student_mobile().casefold() ==
-            user["mobile"].casefold())
-    with allure.step("Проверка корректной даты рождения"):
-        assert (practice_form.read_result_modal_student_date_of_birth().casefold() ==
-            f"{user['dateofbirth'][0]} {user['dateofbirth'][1]},"
-            f"{user['dateofbirth'][2]}".casefold())
-    with allure.step("Проверка корректных дисциплин"):
-        assert (practice_form.read_result_modal_student_subjects().casefold() ==
-            ", ".join(user['subjects']).casefold())
-    with allure.step("Проверка корректного изображения"):
-        assert (practice_form.read_result_modal_student_picture().casefold() ==
-            user["picture"].casefold())
-    with allure.step("Проверка корректного адреса"):
-        assert (practice_form.read_result_modal_student_address().casefold() ==
-            user["address"].casefold())
-    with allure.step("Проверка корректных штата и города"):
-        assert (practice_form.read_result_modal_state_and_city().casefold() ==
-            f"{user['state']} {user['city']}".casefold())
-
-'''
+    with allure.step("Проверка наличия алерта с текстом 'Message received!'"):
+        alert = practice_form.driver.switch_to.alert
+        assert "Message received!" in alert.text
