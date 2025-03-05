f5bf1a72765e8141df42a867e38e533c4f69e4b7
John Doe <kamkilljoy@gmail.com>
2025-03-05 12:56:12 +0400

    перенесено. Тестовые данные актуализированы под задание.

---
+++ test_data/users.py
@@ -0,0 +1,10 @@
+Vasia = {
+    "firstname": "Васисуалий",
+    "password": "admin",
+    "drinks": ("Milk", "Coffee"),
+    "colours": ("Yellow",),
+    "DYL": "No",
+    "email": "vasia@proton.ru",
+}
+
+
